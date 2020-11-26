5af5c0aa68a8338284fe9e72810cf9cdb4f1ed1e
JunSeok Lee <shawn950303@gmail.com>
2020-11-26 21:09:52 +0900

    Create GameManager, Add Random Obstacle Generater

---
+++ controllers/game_manager.py
@@ -0,0 +1,59 @@
+from models.model import *
+from controllers.scene import *
+import random
+from enum import Enum
+
+STAGE_NUM = 1
+GAME_TIME = 20
+OBST_INTERVAL = 3
+
+OBST_NUM = 2
+
+class Obstacle(Enum):
+    DOOLI = 0
+    DOUNER = 1
+
+class GameManager:
+
+    def __init__(self):
+        self.curr_stage = 0
+        self.stage_list = []
+        for i in range(STAGE_NUM):
+            stage = CircusScene()
+            self.stage_list.append(stage)
+
+    def init_stage(self, curr_stage):
+        stage = self.stage_list[curr_stage]
+
+        stage.add_user(User(stage))
+        stage.add_landscape(Landscape(stage))
+        
+        if curr_stage == 0:
+            self.obstacle_random_generator_1(curr_stage)
+        else:
+            pass
+
+    def obstacle_random_generator_1(self, curr_stage):
+        stage = self.stage_list[curr_stage]
+
+        for time_slice in range(0, GAME_TIME, OBST_INTERVAL):
+            variation = random.randrange(3) - 1
+            timing = time_slice - variation
+
+            obstacle = random.randrange(OBST_NUM)
+            height = random.randrange(1, 3) * 100
+
+            if obstacle == Obstacle.DOOLI.value:
+                print("dooli")
+                stage.add_obstacle(Dooli(stage, y=height, start_time=timing))
+            elif obstacle == Obstacle.DOUNER.value:
+                print("douner")
+                stage.add_obstacle(Douner(stage, y=height, start_time=timing))
+            else:
+                pass
+
+    def set_curr_stage(self, curr_stage):
+        self.curr_stage = curr_stage
+
+    def start_game(self):
+        startGame(self.stage_list[0])
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from models.model import *
-from controllers.scene import *
-
+from controllers.game_manager import *
 
 setGameOption(GameOption.INVENTORY_BUTTON, False)
 setGameOption(GameOption.MESSAGE_BOX_BUTTON, False)
 setGameOption(GameOption.ROOM_TITLE, False)
 
-scene = CircusScene()
-scene.add_user(User(scene))
-scene.add_obstacle(Dooli(scene, start_time=1))
-scene.add_obstacle(Douner(scene, y=200, start_time=4))
-scene.add_obstacle(Douner(scene, start_time=7))
-scene.add_obstacle(Dooli(scene, start_time=10))
-scene.add_obstacle(Dooli(scene, y=200, start_time=14))
-scene.add_landscape(Landscape(scene))
+curr_stage = 0
 
-startGame(scene)
+game_manager = GameManager()
+game_manager.init_stage(curr_stage)
+game_manager.start_game()
